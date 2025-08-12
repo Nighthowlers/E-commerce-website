2af672bf49e367e4f142011c526b101817ad7ff2
Nighthowlers <mishrapranjal0307@gmail.com>
2025-08-12 20:03:17 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/E-commerce-website/', // Match your repo name
-  build: {
-    outDir: 'dist',
-  },
+  base: '/e-commerce-website/',
 });
